c4ca6676c6d8e376ab1c1171f2142bc2707ec7bb
Kyron Smith <kds060902@outlook.com>
2022-12-27 15:37:05 +0000

    added detailed get functionallity for recipes of meals

--- src/pages/recipe/recipe.js
+++ src/pages/recipe/recipe.js
@@ -4,14 +4,25 @@ import "./Recipe.css";
 
 export default function Recipe() {
   const { id } = useParams();
-  const url = 'http://localhost:3000/recipes/' + id
-  const { error, isPending, data: recipe } = useFetch(url)
+  const url = "http://localhost:3000/recipes/" + id;
+  const { error, isPending, data: recipe } = useFetch(url);
 
   return (
-  <div className="recipe">
-    {error && <p className="error">{error}</p>}
-    {isPending && <p className="loading">Loading...</p>}
-    {recipe && <h1>{recipe.title}</h1>}
-  </div>
-  )
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
