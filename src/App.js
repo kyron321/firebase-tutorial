a7e19bfbd79b7ee74f56060345b4d470f12ec2b0
Kyron Smith <kds060902@outlook.com>
2022-12-27 14:45:48 +0000

    added navbar with linking functionality

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
 import Search from "./pages/search/Search";
 import Recipe from "./pages/recipe/Recipe";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
         <Switch>
           <Route exact path="/">
             <Home />
