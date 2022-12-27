081d169df7fed6c433d8e234bc89d2030ca8abc5
Kyron Smith <kds060902@outlook.com>
2022-12-27 16:50:44 +0000

    add search functionallity for multiple items

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,7 +1,22 @@
+import { useLocation } from "react-router-dom"
+import RecipeList from "../../components/RecipeList"
+import { useFetch } from "../../hooks/useFetch"
 import "./Search.css"
 
 export default function Search() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get("q")
+
+  const url = "http://localhost:3000/recipes?q=" + query
+  const {error, isPending, data} = useFetch(url)
+
   return (
-    <div>search</div>
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error"></p>}
+      {isPending && <p className="loading">loading...</p>}
+      {data && <RecipeList recipes={data}/>}
+    </div>
   )
 }
