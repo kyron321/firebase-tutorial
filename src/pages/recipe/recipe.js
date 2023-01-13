40875f81a71a5bdcbb9676435a6dd98f2583859d
Kyron Smith <kds060902@outlook.com>
2023-01-13 21:06:37 +0000

    feat: add firebase fetch to singular recipe page

--- src/pages/recipe/recipe.js
+++ src/pages/recipe/recipe.js
@@ -1,11 +1,28 @@
 import { useParams } from "react-router-dom";
-import { useFetch } from "../../hooks/useFetch";
+
 import "./Recipe.css";
+import { useEffect, useState } from "react";
+import { projectFirestore } from "../../firebase/config";
 
 export default function Recipe() {
   const { id } = useParams();
-  const url = "http://localhost:3000/recipes/" + id;
-  const { error, isPending, data: recipe } = useFetch(url);
+
+  const [recipe,setRecipe] = useState(null)
+  const[isPending,setIsPending] = useState(false)
+  const [error,setError] = useState(false)
+
+  useEffect(()=>{
+    setIsPending(true)
+    projectFirestore.collection("recipes").doc(id).get().then((doc)=>{
+      if(doc.exists){
+        setIsPending(false)
+        setRecipe(doc.data())
+      }else{
+        setIsPending(false)
+        setError("could not find recipe")
+      }
+    })
+  },[id])
 
   return (
     <div className="recipe">
