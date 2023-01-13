2d16ce264c5ff0101f4760e851bb1f77f64871bd
Kyron Smith <kds060902@outlook.com>
2023-01-13 21:22:25 +0000

    feat: add basic delete functionality - no realtime data collection

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom"
 import "./RecipeList.css"
+import Trashcan from "../assets/trashcan.svg"
+import { projectFirestore } from "../firebase/config"
+
 
 export default function RecipeList({recipes}) {
 
   if(recipes.length === 0){
     return <div className="error">No recipes to load :(</div>
   }
+
+const handleClick= (id)=>{
+  projectFirestore.collection("recipes").doc(id).delete()
+}
 
   return (
     <div className="recipe-list">
@@ -15,6 +22,7 @@ export default function RecipeList({recipes}) {
                 <p>{recipe.cookingTime} to make.</p>
                 <div>{recipe.method.substring(0,100)}...</div>
                 <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+                <img src={Trashcan} className="delete" onClick={()=> handleClick(recipe.id)} alt="bin"></img>
             </div>
         ))}
     </div>
